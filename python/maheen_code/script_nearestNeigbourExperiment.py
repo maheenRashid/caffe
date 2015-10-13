5ed4d017c79d93fbbad3148ec4701e2ff36aa973
maheenRashid <mhnrashid@gmail.com>
2015-10-13 10:39:38 -0700

    messy code with downloading and formatting non-training data

--- python/maheen_code/script_nearestNeigbourExperiment.py
+++ python/maheen_code/script_nearestNeigbourExperiment.py
@@ -1,6 +1,8 @@
+
 import os;
 import sys;
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir)));
+import re
 import subprocess
 import numpy as np;
 import random;
@@ -64,6 +66,35 @@ def randomlySelectTestSet(path_to_val,val_gt_file,no_classes=50,no_im=5):
     gt_class_chosen=gt_classes[idx_chosen_images];
 
     return zip(im_list_chosen,gt_class_chosen);
+
+def runClassificationTestSet(test_set,out_dir,path_to_classify,gpu_no,layers,ext='JPEG',central_crop=True):
+
+    if not os.path.exists(out_dir):
+        os.mkdir(out_dir);
+    
+    # test_set=randomlySelectTestSet(path_to_val,val_gt_file,no_classes=no_classes,no_im=no_im)
+    
+    out_file= time.strftime("%Y%m%d%H%M%S");
+    pickle.dump([test_set,layers],open(os.path.join(out_dir,out_file+'.p'),'wb'));
+    
+    temp_dir=out_dir+'_temp';
+    if os.path.exists(temp_dir):
+        shutil.rmtree(temp_dir)
+    os.mkdir(temp_dir);
+
+    for im_path,gt_val in test_set:
+        file_name=im_path[im_path.rfind('/')+1:]
+        shutil.copy(im_path,os.path.join(temp_dir,file_name));
+
+    out_file=os.path.join(out_dir,out_file);
+    command=[os.path.join(path_to_classify,'classify.py'),temp_dir,out_file,'--ext',ext,'--gpu',str(gpu_no),'--layer']+layers;
+
+    if central_crop:
+        command=command+["--center_only"]
+    command_formatted=' '.join(command);
+    subprocess.call(command_formatted, shell=True)
+    return out_file;
+
 
 def runClassification(path_to_val,out_dir,val_gt_file,path_to_classify,gpu_no,layers,no_classes=50,no_im=5,ext='JPEG',central_crop=True):
 
@@ -134,9 +165,8 @@ def createImageAndCaptionGrid(img_paths,gt_labels,indices,text_labels):
                 captions[r][c+1]='';
     return im_paths,captions
 
-def main():
-    path_to_classify='..';
-    
+def script_classificationOld():
+
     # return
     path_to_val='/disk2/imagenet/val';
     out_dir='/disk2/octoberExperiments/nn_trained_vs_notrained';
@@ -180,6 +210,95 @@ def main():
         im_paths,captions=createImageAndCaptionGrid(img_paths,gt_labels,indices,text_labels)
         writeHTML(file_name_l+'.html',im_paths,captions)
 
+def script_createBigGTFile():
+    path_to_xml='/disk2/imagenet/structure_files/structure_released.xml';
+    with open(path_to_xml,'rb') as f:
+        content=f.read();
+    content=content.split('<');
+
+    path_to_big_gt='/disk2/imagenet/structure_files/id_to_text_desc.txt';
+    with open(path_to_big_gt,'wb') as f:
+        for c in content:
+            if c.startswith('synset wnid="n'):
+                c=re.split('[=]',c);
+                # print c
+                id=c[1].rsplit(' ',1)[0].strip('"');
+                words=c[2].rsplit(' ',1)[0].strip('"');
+                # print id, words;
+                f.write(id+' '+words+'\n');
+    
+def main():
+    # path_to_val='/disk2/imagenet/val';
+    out_dir='/disk2/octoberExperiments/nn_trained_vs_notrained';
+    path_to_im='/disk2/imagenet/not_trained_im/'
+    ids_meta=pickle.load(open('select_ids_meta.p','rb'));
+    path_to_gt='/disk2/imagenet/structure_files/id_to_text_desc.txt';
+    
+    with open(path_to_gt,'rb') as f:
+        gt_labels=f.readlines();
+    gt_labels=[gt_label.split(' ')[0] for gt_label in gt_labels];
+
+    iter=0;
+    test_set=[]
+    ids=ids_meta[iter];
+    print len(ids)
+    list_of_files=os.listdir(path_to_im);
+    for id in ids:
+            
+        im_list=glob.glob(os.path.join(path_to_im,id+'_*.JPEG'));
+        print len(im_list);
+        idx=gt_labels.index(id);
+        # print gt_labels[:10];
+        im_list=[(path_curr,idx) for path_curr in im_list];
+        test_set.extend(im_list);
+
+    print len(test_set)
+    print test_set[:10];
+    return
+
+
+    no_classes=100;
+    no_im=50;
+    gpu_no=1;
+    # val_gt_file='/disk2/imagenet/ILSVRC2012_devkit_t12/data/ILSVRC2012_validation_ground_truth.txt'
+    val_gt_file='../../data/ilsvrc12/val.txt'
+    file_text_labels='../../data/ilsvrc12/synset_words.txt'
+    ext='JPEG';
+    layers=['pool5','fc6','fc7'];
+    central_crop=True;
+    
+    text_labels= np.loadtxt(file_text_labels, str, delimiter='\t')
+    print len(text_labels)
+    return
+    test_set=randomlySelectTestSet(path_to_val,val_gt_file,no_classes=no_classes,no_im=no_im)
+    
+    file_name=runClassification(path_to_val,out_dir,val_gt_file,path_to_classify,gpu_no,layers,no_classes=no_classes,no_im=no_im,ext=ext,central_crop=central_crop);
+
+    # file_name=os.path.join(out_dir,'20151009153119');
+    # file_name=os.path.join(out_dir,'20151009172027');
+    # file_name=os.path.join(out_dir,'20151009202006');
+    # file_name=os.path.join(out_dir,'20151010000109');
+    test_set,_=pickle.load(open(file_name+'.p','rb'));
+    vals=np.load(file_name+'.npz');
+
+    test_set=sorted(test_set,key=lambda x: x[0])
+    test_set=zip(*test_set);
+    
+    img_paths=list(test_set[0]);
+    gt_labels=list(test_set[1]);
+    
+    numberOfN=5;
+
+    for layer in layers:
+
+        file_name_l=file_name+'_'+layer;
+        indices,conf_matrix=doNN(img_paths,gt_labels,vals[layer],numberOfN=numberOfN,distance='cosine',algo='brute')
+        pickle.dump([img_paths,gt_labels,indices,conf_matrix],open(file_name_l+'.p','wb'));
+        img_paths=[x.replace('/disk2','../..') for x in img_paths];
+        im_paths,captions=createImageAndCaptionGrid(img_paths,gt_labels,indices,text_labels)
+        writeHTML(file_name_l+'.html',im_paths,captions)
+
+    
 
 if __name__=='__main__':
     main();
---
+++ python/maheen_code/script_practiceTree.py
@@ -0,0 +1,177 @@
+# from treelib import Node, Tree
+import re
+import numpy as np
+import random
+# def pickNoOverlapClasses(numClasses,parentChild):
+import urllib2
+import pickle
+import os
+import urllib
+
+def getCounts(total_ids_str,per_class_urls):
+	total_ids_count=[];
+	for id in total_ids_str:
+		url_curr=per_class_urls+id;
+		f=urllib2.urlopen(url_curr);
+		number_images=len(f.readlines());
+		total_ids_count.append(number_images);
+	return np.array(total_ids_count)
+
+
+
+def main():
+
+	val_data_ids='/home/maheenrashid/Downloads/caffe/caffe-rc2/data/ilsvrc12/synsets.txt';
+	is_file='/disk2/imagenet/structure_files/is_a.txt';
+	out_dir='/disk2/imagenet/not_trained_im'
+
+	list_of_nets='http://www.image-net.org/api/text/imagenet.synset.obtain_synset_list';
+	per_class_urls='http://www.image-net.org/api/text/imagenet.synset.geturls?wnid=';
+	# f = urllib2.urlopen(list_of_nets)
+	# total_ids = f.readlines()
+	# total_ids=[id.strip('\n') for id in total_ids]
+
+	# counts=[];
+	# for idx,id in enumerate(total_ids):
+	# 	url_curr=per_class_urls+id;
+	# 	f=urllib2.urlopen(url_curr);
+	# 	number_images=len(f.readlines());
+	# 	counts.append(number_images);
+	# 	if idx==10:
+	# 		break;
+
+	# print total_ids[:10];
+	# print counts[:10];
+	# # with open(list_of_nets,'rb') as f:
+	# # 	nets_list=f.readlines();
+
+	# # print nets_list[:10];
+
+	# return
+
+
+	with open(is_file,'rb') as f:
+		content=f.readlines();
+
+	parents=[];
+	children=[];
+	# total_ids_str=[];
+	for c in content:
+		c_split=c.split(' ');
+		
+		# total_ids_str.append(c_split[0]);
+		# total_ids_str.append(c_split[1].strip('\n'));
+
+		parents.append(int(c_split[0].strip('n')));
+		children.append(int(c_split[1].strip('\n').strip('n')));
+
+	couples=np.array([parents,children]);
+
+	[total_ids_count,total_ids_str,total_ids]=pickle.load(open('number_images.p','rb'));
+	
+	total_ids_count=np.array(total_ids_count)
+	idx_to_keep=np.where(total_ids_count>=50)[0];
+	
+	total_ids=total_ids[idx_to_keep];
+	total_ids_str=total_ids_str[idx_to_keep];
+	total_ids_count=total_ids_count[idx_to_keep];
+	print total_ids.shape,total_ids_str.shape,total_ids_count.shape
+	print total_ids[:10]
+	print total_ids_str[:10]
+	print total_ids_count[:10];
+
+	# return
+	# total_ids=np.unique(couples);
+	# total_ids_str=np.unique(total_ids_str);
+
+	# # total_ids_str=total_ids_str[:100]
+	# print len(total_ids_str)
+	# total_ids_count=[];
+	# for idx,id in enumerate(total_ids_str):
+	# 	if idx%100==0:
+	# 		print idx,len(total_ids_str);
+	# 	url_curr=per_class_urls+id;
+	# 	f=urllib2.urlopen(url_curr);
+	# 	number_images=len(f.readlines());
+	# 	total_ids_count.append(number_images);
+
+	# pickle.dump([total_ids_count,total_ids_str,total_ids],open('number_images.p','wb'));
+
+	# return
+
+	# print len(total_ids_str)
+	# total_ids_str=total_ids_str[np.array(total_ids_count)>=50];
+	# print len(total_ids_str);
+
+	
+	# return
+	# with open(val_data_ids,'rb') as f:
+	# 	val_ids=f.readlines()
+
+	# val_ids=[int(val.strip('\n').strip('n')) for val in val_ids];
+	# val_ids=np.array(val_ids);
+
+	# no_val=np.setdiff1d(total_ids,val_ids);
+	
+	# noClasses=100;
+	# select_ids_meta=[];
+	# for iter in range(5):
+	# 	while (True):
+	# 		random_idx=random.sample(range(no_val.shape[0]),noClasses)
+
+	# 		select_ids=no_val[random_idx];
+	# 		par_int=np.in1d(couples[0],select_ids);
+	# 		child_int=np.in1d(couples[1],select_ids);
+			
+	# 		both_int=par_int+child_int==2;
+			
+	# 		select_ids_str=total_ids_str[[np.where(total_ids==select_id)[0] for select_id in select_ids]]
+	# 		select_ids_str=select_ids_str.ravel();
+	# 		print select_ids_str.shape,select_ids_str[:10]
+	# 		counts=total_ids_count[[np.where(total_ids==select_id)[0] for select_id in select_ids]]
+	# 		counts=np.ravel(counts);
+	# 		# counts=getCounts(select_ids_str,per_class_urls);
+	# 		print sum(counts<50);
+
+
+	# 		if both_int.sum()==0 and sum(counts<50)==0:
+	# 			break;
+
+	# 	select_ids_meta.append(select_ids_str)
+
+	# pickle.dump(select_ids_meta,open('select_ids_meta.p','wb'));
+	# return
+	select_ids_meta=pickle.load(open('select_ids_meta.p','rb'));
+	if not os.path.exists(out_dir):
+		os.mkdir(out_dir);
+
+	for idx_bunch,select_ids_str in enumerate(select_ids_meta):
+		print idx_bunch
+		for select_id in select_ids_str:
+			print select_id,
+			curr_url=per_class_urls+select_id;
+			f = urllib2.urlopen(curr_url)
+			image_links = f.readlines()
+			image_links = [id.strip('\n').strip('\r') for id in image_links]
+			f.close();
+			print len(image_links);				
+			random.shuffle(image_links);
+			out_file_pre=select_id+'_';
+			saved=0;
+			for idx,image_link in enumerate(image_links):
+				out_file_curr=os.path.join(out_dir,out_file_pre+str(idx)+'.JPEG');
+				try:
+					urllib.urlretrieve(image_link,out_file_curr);
+				except:
+					continue;
+				saved+=1;
+				if saved==50:
+					break;
+
+			
+
+
+	# for select_id in select_ids:
+	# 	pass;
+if __name__=='__main__':
+	main();
